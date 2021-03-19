0d2f7d3beb89649a38c2e12126763e499a478c6f
thorne-lauren <laelthorne@gmail.com>
2021-03-18 18:07:21 -0600

    getting started

---
+++ Models/Documentation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeDocumentation.Models
+{
+    public enum Type
+    {
+
+    }
+    
+    public class Documentation
+    {
+    }
+}
---
+++ Models/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace EmployeeDocumentation.Models
+{
+    public class Employee
+    {
+        public int ID { get; set; }
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public DateTime HireDate { get; set; }
+        public int Extension { get; set; }
+        public int SupervisorID { get; set; }
+
+        public ICollection<Documentation> Documentation { get; set; }
+    }
+}
