d92d9bc8cfba1a7f0fc46be83e470426834d151a
thorne-lauren <laelthorne@gmail.com>
2021-04-02 13:43:01 -0600

    Database context

---
+++ Data/TeamContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EmployeeDocumentation.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeDocumentation.Data
+{
+    public class TeamContext : DbContext
+    {
+        public TeamContext(DbContextOptions<TeamContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Supervisor> Supervisors { get; set; }
+        public DbSet<Documentation> Documentation { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Supervisor>().ToTable("Supervisor");
+            modelBuilder.Entity<Documentation>().ToTable("Documentation");
+            modelBuilder.Entity<Employee>().ToTable("Employee");
+        }
+    }
+}
